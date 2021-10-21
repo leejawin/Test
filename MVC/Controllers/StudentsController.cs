2cf8c7247d21dada0cf11fd7809ae8c4d3c17b83
LeejaDevasia <92917956+LeejaDevasia@users.noreply.github.com>
2021-10-22 05:07:50 +1300

    Files Upload

---
+++ MVC/Controllers/StudentsController.cs
@@ -0,0 +1,59 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace MVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index(int? page)
+        {
+            IEnumerable<MvcStudentsModel> studList;
+            System.Net.Http.HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Students").Result;
+            studList = response.Content.ReadAsAsync<IEnumerable<MvcStudentsModel>>().Result.ToPagedList(page?? 1,10);
+            return View(studList);
+        }
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if(id==0)
+            return View(new MvcStudentsModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Students/"+ id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MvcStudentsModel>().Result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(MvcStudentsModel stud)
+        {
+            if (stud.StudentID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Students", stud).Result;
+                TempData["SuccessMessage"] = "Add New Student Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Students/"+stud.StudentID, stud).Result;
+                TempData["SuccessMessage"] = "Details Updated Successfully";
+
+            }
+           
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Students/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Student Details Successfully";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Models/MvcStudentsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class MvcStudentsModel
+    {
+        public int StudentID { get; set; }
+          [Required(ErrorMessage = "Please enter student name.")]        
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Age is required.")]
+        public Nullable<int> Age { get; set; }
+        [Required(ErrorMessage = "Subject is required.")]
+        public string Subject { get; set; }
+        [Display(Name = "Mobile Number:")]
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Please Enter Valid 10 Digit Mobile Number.")]
+        public Nullable<int> Mobile { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Please Enter valid email ID")]
+        public string Email { get; set; }
+    }
+}
